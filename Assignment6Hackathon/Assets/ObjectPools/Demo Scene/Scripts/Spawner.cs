4f769651b45c1339be9dba4a6430c20a350f69b7
sebolsita <sebolsita@int.pl>
2024-01-23 04:09:38 +0000

    New NPC in
    -Added new NPC model (PlayerReadyMe)
    -Added table and chair asset
    -Added bottles assets
    -Added spawner (object pool)
    
    @todo
    -Make NPC static
    -Rotate towards player (worked  half of the day on smooth way to rotate head and the body, didn't work in AR..)
    -Add hands
    -Add sound effects to bottles
    -Make bottles grabable

---
+++ Assignment6Hackathon/Assets/ObjectPools/Demo Scene/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using MidniteOilSoftware.ObjectPoolManager;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _prefabs;
+    [SerializeField][Range(0f, 5f)] private float _spawnDelay = 0.1f;
+    [SerializeField][Range(10f, 30f)] private float _launchForce = 20;
+
+    [SerializeField] private Vector3 _spawnPosition;
+    [SerializeField] private float _randomizationRangeX = 1.0f; // Range for randomization around X
+    [SerializeField] private float _randomizationRangeZ = 1.0f; // Range for randomization around Z
+
+    private int _spawnedObjectCount = 0;
+
+    [SerializeField] private TextMeshPro _objectCountText;
+
+    private void Awake()
+    {
+        _spawnPosition = transform.position;
+    }
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(SpawnRandomObject), 0f, _spawnDelay);
+    }
+
+    private void SpawnRandomObject()
+    {
+        GameObject prefab = _prefabs[Random.Range(0, _prefabs.Count)];
+
+        // Randomize X and Z around the set position
+        float randomX = Random.Range(-_randomizationRangeX, _randomizationRangeX);
+        float randomZ = Random.Range(-_randomizationRangeZ, _randomizationRangeZ);
+
+        Vector3 spawnPosition = _spawnPosition + new Vector3(randomX, 0f, randomZ);
+
+        GameObject go = ObjectPoolManager.SpawnGameObject(prefab, spawnPosition, Quaternion.identity);
+        go.GetComponent<Rigidbody>()?.AddForce(transform.up * _launchForce, ForceMode.Impulse);
+
+        _spawnedObjectCount++;
+
+        // Update TextMeshPro with the new count
+        if (_objectCountText != null)
+        {
+            _objectCountText.text = "Bottles: " + _spawnedObjectCount.ToString();
+        }
+    }
+}
