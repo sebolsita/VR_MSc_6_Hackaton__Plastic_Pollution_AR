a92212fcdf7a6dca66d73212b74cbda6d8e459ea
sebolsita <sebolsita@int.pl>
2024-01-26 07:02:24 +0000

    Adjusted bootle spawner
    -it should make bigger impact now
    *probably will need to be fixed in ar in terms of perfomance

--- Assignment6Hackathon/Assets/ObjectPools/Demo Scene/Scripts/Spawner.cs
+++ Assignment6Hackathon/Assets/ObjectPools/Demo Scene/Scripts/Spawner.cs
@@ -7,8 +7,8 @@ using Random = UnityEngine.Random;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _prefabs;
-    [SerializeField][Range(0f, 5f)] private float _spawnDelay = 0.1f;
-    [SerializeField][Range(10f, 30f)] private float _launchForce = 20;
+    [SerializeField][Range(0f, 5f)] private float _spawnDelay = 0.01f;
+    [SerializeField][Range(1f, 30f)] private float _launchForce = 10;
 
     [SerializeField] private Vector3 _spawnPosition;
     [SerializeField] private float _randomizationRangeX = 1.0f; // Range for randomization around X
@@ -46,7 +46,7 @@ public class Spawner : MonoBehaviour
         // Update TextMeshPro with the new count
         if (_objectCountText != null)
         {
-            _objectCountText.text = "Bottles: " + _spawnedObjectCount.ToString();
+            _objectCountText.text = "Bottles: " + (_spawnedObjectCount * 5).ToString();
         }
     }
 }
