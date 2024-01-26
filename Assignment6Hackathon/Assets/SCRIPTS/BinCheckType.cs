2620d81d075516e8ff839d5234946b76c4e60918
sebolsita <sebolsita@int.pl>
2024-01-26 03:25:09 +0000

    Fixed male npc animation for the minigame
    -changed the head mask
    -enabling the head for animations then switching to track player
    -changed layout of the bins, 2 cat. PLASTIC and GENERAL, added proper tags for current assets

--- Assignment6Hackathon/Assets/SCRIPTS/BinCheckType.cs
+++ Assignment6Hackathon/Assets/SCRIPTS/BinCheckType.cs
@@ -1,19 +1,49 @@
+using System.Collections;
+using System.Drawing.Text;
 using UnityEngine;
 
 public class BinCollision : MonoBehaviour
 {
     public string correctPlasticType; // Assign this in the inspector
+    public Animator npcAnimatorMale;
+    public Animator npcAnimatorFemale;
+    public int headLayerIndex; // Set this to the index of the head layer in the Animator
+
 
     void OnCollisionEnter(Collision collision)
     {
+        int howMuchError = 0;
         // Check if the collided object has the correct plastic type tag
         if (collision.gameObject.CompareTag(correctPlasticType))
         {
             Debug.Log("Bin (" + gameObject.tag + ") registered correct trash (" + correctPlasticType + ")");
+            npcAnimatorMale.SetLayerWeight(headLayerIndex, 0);
+            npcAnimatorMale.SetTrigger("StandingYes");
+            StartCoroutine(EnableLayerAfterDelay(2));
+
         }
         else
         {
             Debug.Log("Bin (" + gameObject.tag + ") registered wrong trash (" + collision.gameObject.tag + ")");
+            howMuchError = howMuchError+1;
+            if (howMuchError < 2)
+            {
+                npcAnimatorMale.SetLayerWeight(headLayerIndex, 0);
+                npcAnimatorMale.SetTrigger("StandingNo");
+                StartCoroutine(EnableLayerAfterDelay(2));
+            }
+            else
+            {
+                npcAnimatorMale.SetLayerWeight(headLayerIndex, 0);
+                npcAnimatorMale.SetTrigger("StandingVeryNo");
+                StartCoroutine(EnableLayerAfterDelay(2));
+            }
         }
+    }
+
+    IEnumerator EnableLayerAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        npcAnimatorMale.SetLayerWeight(headLayerIndex, 1);
     }
 }
